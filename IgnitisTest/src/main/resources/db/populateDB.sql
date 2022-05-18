efb5dad34a732946dedac16e4fb7b1550ed317b6
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-18 11:02:04 +0300

    Update populateDB.sql

--- IgnitisTest/src/main/resources/db/populateDB.sql
+++ IgnitisTest/src/main/resources/db/populateDB.sql
@@ -8,5 +8,5 @@ INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
 INSERT INTO posts (user_id, title, text)
-VALUES (100000, 'User Post_1', 'User Hello'),
-       (100001, 'Admin Post_2', 'Best admin hey');
+VALUES (100000, 'User Post_1','User Hello'),
+       (100001, 'Admin Post_2','Best admin hey');
